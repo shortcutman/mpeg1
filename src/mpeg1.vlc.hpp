6863d4fb7ad328f15557a24a50b07c44a8d5d188
Daniel Burke <danieljburke@me.com>
2025-08-08 16:57:22 +1000

    Adding more tables.

--- src/mpeg1.vlc.hpp
+++ src/mpeg1.vlc.hpp
@@ -9,6 +9,7 @@
 
 namespace mpeg1 {
 
+// Table B.1 Variable length codes for macroblock_address_increment
 const VariableLengthCode<size_t> MACROBLOCK_ADDRESSING = {{
     {0b1, 1, 1},
     {0b011, 3, 2},
@@ -53,9 +54,85 @@ struct MacroblockType {
     bool intra;
 };
 
+// Table 5.2b Variable length codes for macroblock_type in intra-coded pictures.
 const VariableLengthCode<MacroblockType> MACROBLOCK_TYPE_INTRA_VLC = {{
     {0b1, 1, {false, false, false, false, true}},
     {0b01, 2, {true, false, false, false, true}}
+}};
+
+// Table B5.a Variable length codes for dct_dc_size_luminance
+const VariableLengthCode<size_t> BLOCK_DCT_DC_SIZE_LUMINANCE = {{
+    {0b100, 3, 0},
+    {0b00, 2, 1},
+    {0b01, 2, 2},
+    {0b101, 3, 4},
+    {0b1110, 4, 5},
+    {0b11110, 5, 6},
+    {0b111110, 6, 7},
+    {0b1111110, 7, 8},
+}};
+
+// Table B5.b Variable length codes for dct_dc_size_chrominance
+const VariableLengthCode<size_t> BLOCK_DCT_DC_SIZE_CHROMINANCE = {{
+    {0b00, 2, 0},
+    {0b01, 2, 1},
+    {0b10, 2, 2},
+    {0b110, 3, 3},
+    {0b1110, 4, 4},
+    {0b11110, 5, 5},
+    {0b111110, 6, 6},
+    {0b1111110, 7, 7},
+    {0b11111110, 8, 8},
+}};
+
+struct DCTCoeff {
+    int8_t run;
+    int8_t level;
+};
+
+
+const VariableLengthCode<DCTCoeff> BLOCK_DCT_COEFF = {{
+// Table B.5c Variable length codes for dct_coeff_first and dct_coeff_next
+    {0b1, 1, {0, 1}},
+
+    {0b11, 2, {0, 1}},
+
+    {0b011, 3, {1, 1}},
+
+    {0b0100, 4, {0, 2}},
+    {0b0101, 4, {2, 1}},
+
+    {0b00101, 5, {0, 3}},
+    {0b00111, 5, {3, 1}},
+    {0b00110, 5, {4, 1}},
+
+    {0b000110, 6, {1, 2}},
+    {0b000111, 6, {5, 1}},
+    {0b000101, 6, {6, 1}},
+    {0b000100, 6, {7, 1}},
+
+    {0b0000110, 7, {0, 4}},
+    {0b0000100, 7, {2, 2}},
+    {0b0000111, 7, {8, 1}},
+    {0b0000101, 7, {9, 1}},
+
+    {0b00100110, 8, {0, 5}},
+    {0b00100001, 8, {0, 6}},
+    {0b00100101, 8, {1, 3}},
+    {0b00100100, 8, {3, 2}},
+    {0b00100111, 8, {10, 1}},
+    {0b00100011, 8, {11, 1}},
+    {0b00100010, 8, {12, 1}},
+    {0b00100000, 8, {13, 1}},
+
+    {0b0000001010, 10, {0, 7}},
+    {0b0000001100, 10, {1, 4}},
+    {0b0000001011, 10, {2, 3}},
+    {0b0000001111, 10, {4, 2}},
+    {0b0000001001, 10, {5, 2}},
+    {0b0000001110, 10, {14, 1}},
+    {0b0000001101, 10, {15, 1}},
+    {0b0000001000, 10, {16, 1}},
 }};
 
 }
