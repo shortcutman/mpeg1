fa9de9e7be8b295669db9a6a01d1fb67c1a77179
Daniel Burke <danieljburke@me.com>
2025-10-17 14:45:18 +1100

    Clamp output values from YCbCr -> RGB conversion.

--- src/colour.cpp
+++ src/colour.cpp
@@ -9,12 +9,26 @@
 
 using namespace image;
 
+namespace {
+
+inline int adjustAndClamp(float val) {
+    if (val > 255) {
+        return 255;
+    } else if (val < 0) {
+        return 0;
+    } else {
+        return static_cast<int>(val);
+    }
+}
+
+}
+
 // MPEG1 uses ITU-R Recommendation BT.601 https://en.wikipedia.org/wiki/Rec._601
 // Conversion to RGB from https://fourcc.org/fccyvrgb.php
 Colour image::ycbcrToRGB(const Colour& ycbcr) {
-    auto r = 1.164f * (ycbcr.y - 16) + 1.596f * (ycbcr.cr - 128);
-    auto g = 1.164f * (ycbcr.y - 16) - 0.813f * (ycbcr.cr - 128) - 0.391f * (ycbcr.cb - 128);
-    auto b = 1.164f * (ycbcr.y - 16) + 2.018f * (ycbcr.cb - 128);
+    auto r = adjustAndClamp(1.164f * (ycbcr.y - 16) + 1.596f * (ycbcr.cr - 128));
+    auto g = adjustAndClamp(1.164f * (ycbcr.y - 16) - 0.813f * (ycbcr.cr - 128) - 0.391f * (ycbcr.cb - 128));
+    auto b = adjustAndClamp(1.164f * (ycbcr.y - 16) + 2.018f * (ycbcr.cb - 128));
     return Colour{.r = (int)r, .g = (int)g, .b = (int)b};
 }
 
