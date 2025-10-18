79b80f806de7b45c4ccdc7a0d678d9cc3565c642
Daniel Burke <danieljburke@me.com>
2025-10-18 14:36:26 +1100

    Tests for bitutil peek code.

--- src/mpeg1_vid/tests/decode.tests.cpp
+++ src/mpeg1_vid/tests/decode.tests.cpp
@@ -6,6 +6,7 @@
 #include "mpeg1_vid/decode.hpp"
 
 #include "mpeg1.consts.hpp"
+#include "bitspan.hpp"
 
 #include <gtest/gtest.h>
 
@@ -42,4 +43,29 @@ TEST(MPEG1_Decode, get_code_slice) {
     test_code(make_bytes(0x00, 0x00, 0x01, 0x01), mpeg1::start_code::slice_minimum);
     test_code(make_bytes(0x00, 0x00, 0x01, 0x11), uint32_t{0x0111});
     test_code(make_bytes(0x00, 0x00, 0x01, 0xaf), mpeg1::start_code::slice_maximum);
+}
+
+TEST(MPEG1_Decode, peek_code_aligned) {
+    auto peek_code_vector = [](std::vector<std::byte> data) -> bool {
+        util::bitspan bits(data);
+        return mpeg1::peak_code(bits);
+    };
+
+    EXPECT_TRUE(peek_code_vector(make_bytes(0x00, 0x00, 0x01, 0x01)));
+    EXPECT_TRUE(peek_code_vector(make_bytes(0x00, 0x00, 0x01, 0x02)));
+    
+    EXPECT_FALSE(peek_code_vector(make_bytes(0x00, 0x00, 0x02, 0x01)));
+    EXPECT_FALSE(peek_code_vector(make_bytes(0x00, 0x00, 0x00, 0x01, 0x01)));
+}
+
+TEST(MPEG1_Decode, peek_code_unaligned) {
+    auto peek_code_vector = [](std::vector<std::byte> data, int read_bits) -> bool {
+        util::bitspan bits(data);
+        bits.read_bits_be(read_bits);
+        return mpeg1::peak_code(bits);
+    };
+
+    EXPECT_TRUE(peek_code_vector(make_bytes(0x00, 0x00, 0x01, 0x01), 0));
+    EXPECT_TRUE(peek_code_vector(make_bytes(0x00, 0x00, 0x00, 0x01, 0x00), 2));
+    EXPECT_TRUE(peek_code_vector(make_bytes(0x00, 0x00, 0x00, 0x00, 0x01), 9));
 }
