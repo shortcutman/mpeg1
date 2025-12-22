4b825309138052590107c7c77f9f7743a2ff8195
Daniel Burke <danieljburke@me.com>
2025-12-22 16:46:29 +1100

    Copy unit tests for half pel motion vector.

--- src/mpeg1_vid/tests/copy.tests.cpp
+++ src/mpeg1_vid/tests/copy.tests.cpp
@@ -60,3 +60,36 @@ TEST(Copy, copy_block_mv_from_image_movement) {
         }
     }
 }
+
+TEST(Copy, copy_block_mv_from_half_movement) {
+    auto source = image::Frame::make_frame(32, 32);
+    
+    for (size_t y = 0; y < 16; y++) {
+        for (size_t x = 0; x < 16; x++) {
+            source.image[y * 32 + x] = image::Colour{.y = 50};
+        }
+    }
+
+    auto copy_mv_down = mpeg1::copy_block_mv_from_image(2, std::make_tuple(0, -1), source);
+    for (size_t i = 0; i < 16; i++) {
+        image::Colour c{.y = 25};
+        EXPECT_EQ(copy_mv_down[i], c) << "Position: " << i;
+    }
+    for (size_t i = 16; i < 256; i++) {
+        image::Colour c{.r = 0, .g = 0, .b = 0};
+        EXPECT_EQ(copy_mv_down[i], c) << "Position: " << i;
+    }
+
+    auto copy_mv_left = mpeg1::copy_block_mv_from_image(1, std::make_tuple(-1, 0), source);
+    for (size_t y = 0; y < 16; y++) {
+        for (size_t x = 0; x < 16; x++) {
+            if (x < 1) {
+                image::Colour c{.r = 25, .g = 0, .b = 0};
+                EXPECT_EQ(copy_mv_left[y * 16 + x], c) << "Position x: " << x << " y: " << y;
+            } else {
+                image::Colour c{.r = 0, .g = 0, .b = 0};
+                EXPECT_EQ(copy_mv_left[y * 16 + x], c) << "Position x: " << x << " y: " << y;
+            }
+        }
+    }
+}
